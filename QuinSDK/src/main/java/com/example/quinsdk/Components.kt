064e9649b8c7c317ab142a866a00fafaee8f7e28
Sampan Solomon <sampan@quintessential.design>
2023-10-04 11:54:33 +0530

    Initial Commit

---
+++ QuinSDK/src/main/java/com/example/quinsdk/Components.kt
@@ -0,0 +1,9 @@
+package com.example.quinsdk
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun CustomText(text: String, ){
+
+
+}
---
+++ app/src/main/java/com/example/quinsdk/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.example.quinsdk
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.quinsdk.ui.theme.QuinSDKTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            QuinSDKTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    QuinSDKTheme {
+        Greeting("Android")
+    }
+}
